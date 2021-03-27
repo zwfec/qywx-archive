a6c33080d75f71108d25512d4a661543d94845ae
zwfec <zwfec@qq.com>
2021-03-27 17:02:32 +0800

    Update Config.java

--- com/tencent/wework/Config.java
+++ com/tencent/wework/Config.java
@@ -2,13 +2,5 @@ package com.tencent.wework;
 
 public class Config {
     //    企业微信管理后台设置的私钥 PKCS#1 和 PKCS#8 对应使用 方法是不一样的  http://web.chacuo.net/netrsakeypair
-    public String privKey = "-----BEGIN PRIVATE KEY-----\n" +
-            "2QslZXYE5LbKatkBjByjTJgVMQ1h3RgkUok5q4YOVREZQUZWfBgGu24WXpBzn+Fr\n" +
-            "d3/TlfpNkfG5AqFhzHBhjDf6RA+kotQu8zIJEgh9fDdW8hJIjQ/PeJUKuB+tKLx3\n" +
-            "4gPHpzJRQJUAMdyTcJdQYBusrSvcI7LPSggB1rwZvUgdDNHv7P2bJe7V5CrDZary\n" +
-            "Wy40BfsEVVpomPjTbEE+85KAQ4Lgl6Gfbp648S3dJnS+u1R4vuV6oC8KYwhzrS8o\n" +
-            "vybZiXPqAFQeXVsIufrlR8YF7jadRVhGZ2Qf3LM+fuU+ddWiXqvIPOVeu3uednpQ\n" +
-            "dAtbD4vF1NRh3do/I8GzL/399GEaigNNm39iOegAdTtplRN54xSaiWUTrWzSkYir\n" +
-            "0dUly6jVCE+NyY/eUEtEuw==\n" +
-            "-----END PRIVATE KEY-----";
+    public String privKey = "这里是您的私钥";
 }
